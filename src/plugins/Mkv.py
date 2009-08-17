929afa3ef3130914b8be96ebdf56885c38abce5c
elbersb <elbersb@b91be68e-833a-11dd-a143-0dd59a09f4aa>
2009-08-17 16:19:38 +0000

    MKV plugin: slightly better error checking

--- src/plugins/Mkv.py
+++ src/plugins/Mkv.py
@@ -27,10 +27,7 @@ class Mkv(Plugin):
     
     def configurate(self, dialog):
         dialog.vbox.set_spacing(4) 
-
-        # TODO: optionen für hq und avi?
-        # TODO: automatisch umwandeln nach schneiden? (option?)
-
+        
         dialog.vbox.pack_start(gtk.Label("Name von mkvmerge:"), expand=False)
         entry_mkvmerge = gtk.Entry()
         dialog.vbox.pack_start(entry_mkvmerge, expand=False)
@@ -49,20 +46,27 @@ class Mkv(Plugin):
         self.toolbutton.set_sensitive(False)
         filenames = self.gui.main_window.get_selected_filenames()
         self.gui.main_window.set_tasks_visible(True)
+
         self.success = 0
+        self.errors ={}
                 
         def mkvmerge():
-
             for count, filename in enumerate(filenames):           
                 yield 0, count
                 self.progress = 0
                 p = subprocess.Popen([self.Config['mkvmerge'], "-o", os.path.splitext(filename)[0] + ".mkv", filename], stdout=subprocess.PIPE)
-                            
+                p.stdout.readline()
+
+                line = ""                            
                 while p.poll() == None:
                     # read progress from stdout 
                     char = p.stdout.read(1)
+                    line += char
                     progress = ''
                     if char == ':':
+                        if "Error" in line or "Warning" in line:                            
+                            break
+                    
                         while char != '%':
                             char = p.stdout.read(1)
                             progress += char
@@ -72,21 +76,37 @@ class Mkv(Plugin):
                             yield 1, self.progress                                
                         except ValueError:
                             pass
+                
+                exit_code = p.poll()
 
-                if self.progress == 100:
+                if exit_code == 0:
                     self.success += 1
+                else:
+                    error = p.stdout.readline()
+                    try:
+                        error = error.split(":")[1]
+                    except IndexError:
+                        pass
+                        
+                    if "unknown type" in error:
+                        error = "Datei konnte nicht gelesen werden."
+                    self.errors[filename] = error
                   
-        def loop(state, argument):
+        def loop(state, argument):            
             if state == 0:
                 self.gui.main_window.set_tasks_text("In Mkv Umwandeln %s/%s" % (str(argument + 1), str(len(filenames))))
             else:                
                 self.gui.main_window.set_tasks_progress(argument)
         
         def complete():
-            self.gui.main_window.change_status(0, "Erfolgreich %s/%s Dateien umgewandelt." % (str(self.success), str(len(filenames))))
-            self.gui.main_window.set_tasks_visible(False)
+            if len(self.errors) == 0:
+                self.gui.main_window.change_status(0, "Erfolgreich %s/%s Dateien umgewandelt." % (str(self.success), str(len(filenames))))
+            else:
+                self.gui.main_window.change_status(0, "Erfolgreich %s/%s Dateien umgewandelt. (Fehler: %s)" % (str(self.success), str(len(filenames)), " ".join(self.errors.values())))
+            
+            self.gui.main_window.set_tasks_visible(False)                
             if self.success > 0:
                 self.app.show_section(self.app.section)
             self.toolbutton.set_sensitive(True)
                         
-        GeneratorTask(mkvmerge, loop, complete).start()             
+        GeneratorTask(mkvmerge, loop, complete).start() 
