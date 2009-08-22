bcecc6d4a26532e44eaaca7e0fdeacdd47d5e39c
Benjamin Elbers <elbersb@gmail.com>
2009-08-22 14:00:46 +0200

    markdown readme

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-####################################################################   
 OTR-Verwaltung
-Copyright (C) 2009 Benjamin Elbers (elbersb@googlemail.com)
+==============
+
+*Copyright (C) 2009 Benjamin Elbers (elbersb@gmail.com)*
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -14,4 +15,4 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
-####################################################################
+
--- makedeb.sh
+++ makedeb.sh
@@ -14,23 +14,7 @@ tar -xf $name-$version.orig.tar.gz;
 
 cd $name-$version
 
-cat > README << EOF
-OTR-Verwaltung $version
-Copyright (C) 2009 Benjamin Elbers (elbersb@gmail.com)
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-EOF
+cp ../README.md README
 
 dh_make -e elbersb@gmail.com
 rm ../$name-$version.orig.tar.gz
