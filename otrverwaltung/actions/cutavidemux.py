9309a4b14460169499f27be0c10519689d1ce83f
monarc <warcraft99@web.de>
2013-03-08 13:05:04 +0100

    *disable directx in VD *always mux to mkv, if cuts are created with avidemux3

--- otrverwaltung/actions/cutavidemux.py
+++ otrverwaltung/actions/cutavidemux.py
@@ -55,7 +55,7 @@ class CutAvidemux(Cut):
         my_env = os.environ.copy()
         my_env["LANG"] = "C"
         
-        if ".avi" in filename and "avidemux3" in program_config_value and format == Format.HQ or ".avi" in filename and "avidemux3" in program_config_value and format == Format.HD: 
+        if ".avi" in filename and "avidemux3" in program_config_value: 
             try:
                 mkvmerge = subprocess.Popen([self.config.get_program('mkvmerge'),  '--ui-language',  'en_US', '-o', filename+'.mkv', filename], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True,  env=my_env)
                 self.show_progress(mkvmerge)
